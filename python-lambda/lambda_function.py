4e1005986ce0e0a53c5b0f56f89b6811f97556db
antoniehuang <antonieyuchi@gmail.com>
2023-09-02 20:04:09 +0100

    add deploy lambda step

--- python-lambda/lambda_function.py
+++ python-lambda/lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps("Changing the code to see if github action works"),
+        'body': json.dumps("New Python Lambda code"),
         'event': event
     }
